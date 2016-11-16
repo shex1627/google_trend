60884f7a4d4927b4f0b9c198cb6bc126524408d6
shichenghuang <shicheng1627@berkeley.edu>
2016-11-16 11:01:44 -0800

    change prediction name

--- src/generate_prediction.R
+++ src/generate_prediction.R
@@ -9,10 +9,10 @@ source("munge/ds2_setup.R")
 ## ds1
 mod_ts1 <- arima(ts1, order = c(0, 1, 1), seasonal = list(order = c(0,1,1), period = 52))
 pred_ts1 <- predict(mod_ts1, n.ahead = 52)
-write.table(pred_ts1$pred[1:52], "q1_shicheng_huang_25364767.txt", row.names = FALSE, col.names = FALSE)
+write.table(pred_ts1$pred[1:52], "Q1_shicheng_huang_25364767.txt", row.names = FALSE, col.names = FALSE)
 
 ## ds2
 mod_ts2 <- arima(ts2, order = c(0, 1, 1), seasonal = list(order = c(0,1,1), period = 52))
 pred_ts2 <- predict(mod_ts2, n.ahead = 52)
-write.table(pred_ts2$pred[1:52], "q2_shicheng_huang_25364767.txt", row.names = FALSE, col.names = FALSE)
+write.table(pred_ts2$pred[1:52], "Q2_shicheng_huang_25364767.txt", row.names = FALSE, col.names = FALSE)
 
